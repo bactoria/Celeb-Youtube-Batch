b9c0374149d2f620790fd26c9e8d9223e5b08d9b
bactoria <bactoria@gmail.com>
2018-11-29 03:16:51 +0900

    property 수정

--- src/main/java/me/bactoria/celebbatch/UpdateChannelApplication.java
+++ src/main/java/me/bactoria/celebbatch/UpdateChannelApplication.java
@@ -3,11 +3,18 @@ package me.bactoria.celebbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class UpdateChannelApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UpdateChannelApplication.class, args);
+        new SpringApplicationBuilder(UpdateChannelApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "/home/ec2-user/batch/config/real-application.properties";
+
 }
